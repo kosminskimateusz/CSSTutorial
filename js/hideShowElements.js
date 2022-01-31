d4928406df93dff038a01b35f3099e97001dd32a
mk <kosminskimateusz@gmail.com>
2022-01-31 14:33:41 +0100

    Changed js for sideBar

--- js/hideShowElements.js
+++ js/hideShowElements.js
@@ -2,24 +2,22 @@ const sideBar = document.querySelector("#side-bar");
 const showSideBarButton = document.querySelector("#side-bar-button-show");
 const hideSideBarButton = document.querySelector("#side-bar-button-hide");
 
-showSideBarButton.addEventListener('click', showSideBar);
-hideSideBarButton.addEventListener('click', hideSideBar);
-
-
-function showSideBar() {
+const showSideBar = () => {
   sideBar.style.display = 'block';
   showSideBarButton.style.display = 'none';
 }
 
-function hideSideBar() {
+const hideSideBar = () => {
   sideBar.style.display = 'none';
   showSideBarButton.style.display = 'block';
 }
 
+showSideBarButton.addEventListener('click', showSideBar);
+hideSideBarButton.addEventListener('click', hideSideBar);
+
 
 const sideBarLinkHeaders = document.querySelectorAll(".side-bar-link-header");
 const sideBarLists = document.querySelectorAll(".side-bar-list");
-
 
 for (let j = 0; j < sideBarLists.length; j++) {
   let sideBarLinks = sideBarLists[j].querySelectorAll('.side-bar-link');
@@ -31,14 +29,7 @@ for (let j = 0; j < sideBarLists.length; j++) {
   }
 }
 
-
-for (let i = 0; i < sideBarLinkHeaders.length; i++) {
-  sideBarLinkHeaders[i].addEventListener('click', function () {
-    openCloseList(i);
-  }, false);
-}
-
-function openCloseList(number) {
+const openCloseList = (number) => {
   if (sideBarLists[number].style.display != 'block') {
     sideBarLists[number].style.display = 'block';
     sideBarLinkHeaders[number].classList.remove("blue-dot");
@@ -49,6 +40,14 @@ function openCloseList(number) {
     }
   }
 }
+
+
+for (let i = 0; i < sideBarLinkHeaders.length; i++) {
+  sideBarLinkHeaders[i].addEventListener('click', () => {
+    openCloseList(i);
+  }, false);
+}
+
 
 let sideBarLinks = document.querySelectorAll(".side-bar-link");
 
